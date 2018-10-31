cb9eff860d19a4040e0fa15cc4010c6121411c60
bqfix <benjaminfix@gmail.com>
2018-10-31 14:17:27 -0400

    Location Intent
    Created button in MainActivity to point to LocationActivity

--- app/src/main/java/com/example/android/masudatour/MainActivity.java
+++ app/src/main/java/com/example/android/masudatour/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.masudatour;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView helloWorld = (TextView) findViewById(R.id.hello_world);
+        helloWorld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent locationIntent = new Intent(MainActivity.this,LocationActivity.class);
+                startActivity(locationIntent);
+            }
+        });
+
     }
 }
