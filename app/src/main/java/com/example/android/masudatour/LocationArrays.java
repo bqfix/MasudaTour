2b12d5c3c6ec010edfbf2b906400232c8f01b027
bqfix <benjaminfix@gmail.com>
2018-10-31 13:42:41 -0400

    Created LocationArrays
    Created LocationArrays to store and provide access to sets of locations for fragment use

---
+++ app/src/main/java/com/example/android/masudatour/LocationArrays.java
@@ -0,0 +1,33 @@
+package com.example.android.masudatour;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+/**
+ * Class to hold methods to access lists of location for each fragment
+ */
+public class LocationArrays {
+
+    public static ArrayList<Location> getRestaurants(Context context) {
+
+        ArrayList<Location> restaurants = new ArrayList<Location>(Arrays.asList(
+                new Location(context.getString(R.string.sakuraya), context.getString(R.string.sakuraya_descrip), new int[R.drawable.dogs]),
+                new Location(context.getString(R.string.ganesha), context.getString(R.string.ganesha_descrip), new int[R.drawable.dogs]),
+                new Location(context.getString(R.string.yagura), context.getString(R.string.yagura_descrip), new int[R.drawable.dogs]),
+                new Location(context.getString(R.string.kakinoki), context.getString(R.string.kakinoki_descrip), new int[R.drawable.dogs]),
+                new Location(context.getString(R.string.saku), context.getString(R.string.saku_descrip), new int[R.drawable.dogs]),
+                new Location(context.getString(R.string.saku_yakiniku), context.getString(R.string.saku_yakiniku_descrip), new int[R.drawable.dogs]),
+                new Location(context.getString(R.string.yasumasa), context.getString(R.string.yasumasa_descrip), new int[R.drawable.dogs]),
+                new Location(context.getString(R.string.tenshin), context.getString(R.string.tenshin_descrip), new int[R.drawable.dogs]),
+                new Location(context.getString(R.string.kikkouya_udon), context.getString(R.string.kikkouya_udon_descrip), new int[R.drawable.dogs]),
+                new Location(context.getString(R.string.ebisuken), context.getString(R.string.ebisuken_descrip), new int[R.drawable.dogs]),
+        ));
+
+        return restaurants;
+    }
+
+
+}
