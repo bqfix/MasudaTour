663d54dec15cce3c595b8a9eed63b2c2e73ed32b
bqfix <benjaminfix@gmail.com>
2018-10-31 14:01:46 -0400

    LocationFragmentPagerAdapter
    Introduced logic to choose which fragment to display

--- app/src/main/java/com/example/android/masudatour/LocationFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/masudatour/LocationFragmentPagerAdapter.java
@@ -1,4 +1,30 @@
 package com.example.android.masudatour;
 
-public class LocationFragmentPagerAdapter {
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class LocationFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public LocationFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    //Instantiate fragments, passed arguments denote which array to use
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0: return LocationFragment.newInstance("restaurants");
+            case 1: return LocationFragment.newInstance("restaurants");
+            case 2: return LocationFragment.newInstance("restaurants");
+            case 3: return LocationFragment.newInstance("restaurants");
+            default: return null;
+        }
+    }
+
+    //Set number of fragments
+    @Override
+    public int getCount() {
+        return 4;
+    }
 }
