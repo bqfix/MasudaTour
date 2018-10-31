4c3dccd27871dcabb5e46393e7df466cb4168212
bqfix <benjaminfix@gmail.com>
2018-10-31 13:53:02 -0400

    Updated LocationFragment
    Updated LocationFragment logic to adapt an Array of Locations to the ListView

--- app/src/main/java/com/example/android/masudatour/LocationFragment.java
+++ app/src/main/java/com/example/android/masudatour/LocationFragment.java
@@ -1,52 +1,35 @@
 package com.example.android.masudatour;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link LocationFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link LocationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class LocationFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    // Return subject on fragment initialization to decide which arraylist to use
+    private static final String ARG_SUBJECT = "subject";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private String mSubject;
 
     public LocationFragment() {
         // Required empty public constructor
     }
 
     /**
-     * Use this factory method to create a new instance of
+     * Factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param subject of the fragment
      * @return A new instance of fragment LocationFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static LocationFragment newInstance(String param1, String param2) {
+    public static LocationFragment newInstance(String subject) {
         LocationFragment fragment = new LocationFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_SUBJECT, subject);
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,38 +38,39 @@ public class LocationFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mSubject = getArguments().getString(ARG_SUBJECT);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
-    }
+        // Inflate the layout for this fragment
+        View rootView =  inflater.inflate(R.layout.location_list, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+        //Choose the ArrayList
+        final ArrayList<Location> locations;
+        String arrayChoice = mSubject;
+        switch (arrayChoice) {
+            case "restaurants":
+                locations = LocationArrays.getRestaurants(getActivity());
+                break;
+            default:
+                locations = null;
         }
+
+        //Create LocationAdapter
+        LocationAdapter locationAdapter = new LocationAdapter(getActivity(),locations);
+
+        //Find Listview
+        ListView listView = (ListView) getActivity().findViewById(R.id.list);
+
+        //Set adapter to ListView
+        listView.setAdapter(locationAdapter);
+
+        return rootView;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+
+
 }
