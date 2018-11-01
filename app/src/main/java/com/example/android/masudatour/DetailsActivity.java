e034eb5cdb2719885d5e3d05e3dd03b2ef5f071f
bqfix <benjaminfix@gmail.com>
2018-10-31 21:10:09 -0400

    Basic DetailsActivity Logic
    Added logic to set the views in DetailsActivity to reflect the Location

--- app/src/main/java/com/example/android/masudatour/DetailsActivity.java
+++ app/src/main/java/com/example/android/masudatour/DetailsActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.masudatour;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        //Get location from Intent
+        Intent detailsIntent = getIntent();
+        Location location = detailsIntent.getParcelableExtra("location");
+
+        //Set details page to reflect location's details
+        ImageView detailsMainImage = (ImageView) findViewById(R.id.details_main_image);
+        detailsMainImage.setImageResource(location.getImageID()[0]);
+
+        TextView detailsName = (TextView) findViewById(R.id.details_name);
+        detailsName.setText(location.getLocationName());
+
+        TextView detailsDescrip = (TextView) findViewById(R.id.details_descrip);
+        detailsDescrip.setText(location.getLocationDescription());
+
     }
 }
